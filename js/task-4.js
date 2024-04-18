59fdf080a6371fff5335bb6e13d543c9d36b5aa2
Anastasiia Riabokon <a.ryabokon20@gmail.com>
2024-04-18 22:38:36 +0300

    upd task-4

--- js/task-4.js
+++ js/task-4.js
@@ -8,10 +8,10 @@ function getShippingCost(country) {
       return `Shipping to ${country} will cost ${priceChina} credits`;
       break;
     case 'Chile':
-      return `Shipping to ${country} will cost ${priceChina} credits`;
+      return `Shipping to ${country} will cost ${priceChile} credits`;
       break;
     case 'Australia':
-      return `Shipping to ${country} will cost ${priceJamaica} credits`;
+      return `Shipping to ${country} will cost ${priceAustralia} credits`;
       break;
     case 'Jamaica':
       return `Shipping to ${country} will cost ${priceJamaica} credits`;
